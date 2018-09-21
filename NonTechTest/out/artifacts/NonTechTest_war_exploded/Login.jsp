36e3bfe4a8f14641ae70989b076b51e334b5f116
HaleyCYoung <31146929+HaleyCYoung@users.noreply.github.com>
2018-09-20 20:15:23 -0500

    Add files via upload
    login functions with css/html

---
+++ NonTechTest/out/artifacts/NonTechTest_war_exploded/Login.jsp
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>123 Testing</title>
+        <link href='login.css' rel='stylesheet'>
+    </head>
+    <body>
+        <form class="box" method = "post" action = "Login">
+            <h1>CyberSmart</h1>
+             <input type="text" name="username" placeholder="Username" required/>
+             <input type="password" name="password" Placeholder="Password" required/>
+             <input type = "submit" value = "Login"/>
+        </form>
+    </body>
+</html>
+
+
---
+++ NonTechTest/src/Servlets/Test.java
@@ -0,0 +1,35 @@
+package Servlets;
+import UserInfo.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@WebServlet("/Test")
+public class Test extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setHeader("Cache-control", "no-cache, no-store");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Expires", "-1");
+
+        if (request.getSession(false).getAttribute("username")!= null){
+            User user = (User) request.getSession().getAttribute("username");
+              request.getRequestDispatcher("/Linked.jsp").forward(request, response);
+        }
+        else {
+            request.setAttribute("fromURI", request.getRequestURI());
+            RequestDispatcher dispatch = request.getRequestDispatcher("/Login");
+            dispatch.forward(request, response);
+        }
+    }
+}
